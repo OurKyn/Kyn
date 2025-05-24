5be03b00ff76c5a5e468ec9032fac2a4245c1577
bantoinese83 <b.antoine.se@gmail.com>
2025-05-24 15:34:40 -0400

    chore(next): allow openweathermap.org for next/image weather icons

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['www.imghippo.com', 'picsum.photos'],
+    domains: ['www.imghippo.com', 'picsum.photos', 'openweathermap.org'],
   },
   /* config options here */
 }
